6f002ef11f656ec1c47733b7b2bb49b48eac2e36
王佳 <wangjia@cvte.cn>
2015-09-28 11:49:34 +0800

    [feature]init project

---
+++ app/src/main/java/com/jacob/color/picker/MainActivity.java
@@ -0,0 +1,51 @@
+package com.jacob.color.picker;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.larswerkman.holocolorpicker.ColorPicker;
+import com.larswerkman.holocolorpicker.OpacityBar;
+import com.larswerkman.holocolorpicker.SVBar;
+import com.larswerkman.holocolorpicker.SaturationBar;
+import com.larswerkman.holocolorpicker.ValueBar;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ColorPicker picker = (ColorPicker) findViewById(R.id.picker);
+        SVBar svBar = (SVBar) findViewById(R.id.svbar);
+        OpacityBar opacityBar = (OpacityBar) findViewById(R.id.opacitybar);
+        SaturationBar saturationBar = (SaturationBar) findViewById(R.id.saturationbar);
+        ValueBar valueBar = (ValueBar) findViewById(R.id.valuebar);
+
+        picker.addSVBar(svBar);
+        picker.addOpacityBar(opacityBar);
+        picker.addSaturationBar(saturationBar);
+        picker.addValueBar(valueBar);
+
+//To get the color
+        picker.getColor();
+
+//To set the old selected color u can do it like this
+        picker.setOldCenterColor(picker.getColor());
+// adds listener to the colorpicker which is implemented
+//in the activity
+        picker.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
+            @Override
+            public void onColorChanged(int color) {
+
+            }
+        });
+
+//to turn of showing the old color
+        picker.setShowOldCenterColor(false);
+
+//adding onChangeListeners to bars
+    }
+
+}
